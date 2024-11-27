2698a8ae843cf45687c824872d70f3a9949c1b2a
Cynthia-er <2574525350@qq.com>
2024-11-27 20:49:41 +0800

    firstCommit

---
+++ olap_web/src/main/java/com/example/olap_web/OlapWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.olap_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlapWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlapWebApplication.class, args);
+    }
+
+}
